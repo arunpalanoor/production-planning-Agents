e6dd159d5fa53e3ce81487946966b16c3cdb549d
Arun Palanoor <parun.mech@gmail.com>
2025-10-03 16:42:37 +0100

    Delete src/tools directory

--- src/tools/excel_parser.py
+++
@@ -1,7 +0,0 @@
-# src/tools/excel_parser.py
-
-def parse_excel(file_path: str = None):
-    return {
-        "product_A": {"demand": 120, "lead_time": 2},
-        "product_B": {"demand": 80, "lead_time": 3}
-    }
